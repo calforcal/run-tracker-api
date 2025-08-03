fb5e52db43870c4799f08b9446211d93012ee02a
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2025-08-03 17:50:50 -0600

    Update main.go with new services, logging, and configs

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"log"
 	"run-tracker-api/api/handlers/athlete"
+	"run-tracker-api/api/handlers/auth"
 	"run-tracker-api/api/handlers/home"
+	authService "run-tracker-api/internal/auth"
 	"run-tracker-api/internal/config"
+	"run-tracker-api/internal/storage"
 	"run-tracker-api/internal/strava"
+	"run-tracker-api/internal/users"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -19,16 +24,43 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	logger, _ := zap.NewProduction()
+
 	config := config.New()
-	stravaService := strava.New()
+
+	storage := storage.New(config, logger)
+
+	stravaService := strava.New(config, logger)
+	userService := users.New(config, logger, storage)
+	authService := authService.New(config, logger)
 
 	homeHandler := home.New()
-	athleteHandler := athlete.New(config, stravaService)
+	athleteHandler := athlete.New(config, stravaService, logger)
+	authHandler := auth.New(config, stravaService, userService, authService, logger)
 
 	api := e.Group("/api")
 
 	api.GET("/home", homeHandler.Home)
 	api.GET("/athlete", athleteHandler.GetAthlete)
 
+	api.POST("/authorize-user", authHandler.AuthorizeStravaUser)
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			req := c.Request()
+			res := c.Response()
+
+			err := next(c)
+
+			logger.Info("response",
+				zap.Int("status", res.Status),
+				zap.String("method", req.Method),
+				zap.String("uri", req.RequestURI),
+				zap.Error(err),
+			)
+
+			return err
+		}
+	})
 	e.Logger.Fatal(e.Start(":8000"))
 }
